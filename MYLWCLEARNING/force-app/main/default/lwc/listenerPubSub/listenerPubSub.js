b89eb6bf6b39525d9f191ba788450197a5713a25
Jose Arun <a2jj2k@gmail.com>
2020-10-20 17:19:52 +0530

    Communication parent->child, child->parent & b/w 2 components different DOM

---
+++ MYLWCLEARNING/force-app/main/default/lwc/listenerPubSub/listenerPubSub.js
@@ -0,0 +1,22 @@
+import { LightningElement,track,wire} from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import { registerListener, unregisterAllListeners, fireEvent } from 'c/pubsub';
+
+export default class ListenerPubSub extends LightningElement {
+
+    @track message;
+    @wire(CurrentPageReference) pageRef;
+
+    connectedCallback(){
+
+        registerListener('messagesend', this.handlemessagesend, this);
+    }
+
+    handlemessagesend(publisherMessage) {
+        this.message = publisherMessage;
+    }
+
+    disconnectedCallback() {
+        unregisterAllListeners(this);
+    } 
+}
---
+++ MYLWCLEARNING/force-app/main/default/lwc/publisherPubSub/publisherPubSub.js
@@ -0,0 +1,14 @@
+import { LightningElement, wire  } from 'lwc';
+
+import { CurrentPageReference } from 'lightning/navigation';
+import { fireEvent } from 'c/pubsub';
+
+export default class PublisherPubSub extends LightningElement {
+
+    @wire(CurrentPageReference) pageRef;
+
+    publishMessage(event){
+
+        fireEvent(this.pageRef,'messagesend','This message comes from Publisher');
+    }
+}
